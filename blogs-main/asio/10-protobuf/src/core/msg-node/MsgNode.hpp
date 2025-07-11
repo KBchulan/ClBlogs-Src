975cabde692dc5ec671e9702af9a7549b7d2fca7
KB初岚 <145010602+KBchulan@users.noreply.github.com>
2025-07-11 16:31:25 +0800

    update-7-11-2

---
+++ blogs-main/asio/10-protobuf/src/core/msg-node/MsgNode.hpp
@@ -0,0 +1,44 @@
+#ifndef MSGNODE_HPP
+#define MSGNODE_HPP
+
+#include <boost/asio/detail/socket_ops.hpp>
+#include <cstddef>
+#include <cstring>
+
+#include <global/Global.hpp>
+#include <core/CoreExport.hpp>
+#include <winsock2.h>
+
+namespace core {
+
+class CORE_EXPORT MsgNode {
+  friend class Session;
+public:
+  // 发送节点的构造
+  MsgNode(char *data, short max_len) : _max_len(max_len + HEAD_LENGTH) {
+    _data = new char[static_cast<size_t>(_max_len + 1)]();
+    auto head_len = boost::asio::detail::socket_ops::host_to_network_short(static_cast<u_short>(max_len));
+    memcpy(_data, &head_len, HEAD_LENGTH);
+    memcpy(_data + HEAD_LENGTH, data, static_cast<size_t>(max_len));
+    _data[_max_len] = '\0';
+  }
+
+  // 接收节点的构造
+  MsgNode(short max_len) : _max_len(max_len) {
+    _data = new char[static_cast<size_t>(_max_len + 1)]();
+  }
+
+  ~MsgNode() {
+    delete []_data;
+  }
+
+private:
+  short _cur_len{};
+  short _max_len;
+  char* _data;
+};
+
+} // namespace core
+
+
+#endif // MSGNODE_HPP
---
+++ blogs-main/asio/10-protobuf/test/test.cc
@@ -0,0 +1,7 @@
+#include "person.pb.h"
+
+int main() {
+  person::data::Person person;
+  person.set_name("chulan");
+  person.set_id(55);
+}
